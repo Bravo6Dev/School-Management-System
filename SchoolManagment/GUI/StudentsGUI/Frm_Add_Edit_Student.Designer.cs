7ecffeae2403905f1520852b036149d8aa899c75
Hatem <beshinahatem@gmail.com>
2024-08-19 06:35:38 +0200

    Add Student Add and Update Form

---
+++ SchoolManagment/GUI/StudentsGUI/Frm_Add_Edit_Student.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SchoolManagment.GUI.StudentsGUI
+{
+    partial class Frm_Add_Edit_Student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            Panel_Contanier = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(189, 516);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Dock = DockStyle.Top;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Image = Properties.Resources.graduated;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(189, 39);
+            button1.TabIndex = 0;
+            button1.Text = "معلومات الطالب";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Dock = DockStyle.Top;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Image = Properties.Resources.report_card;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 39);
+            button2.Name = "button2";
+            button2.Size = new Size(189, 39);
+            button2.TabIndex = 1;
+            button2.Text = "درجات الطالب";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.Dock = DockStyle.Bottom;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Image = Properties.Resources.back;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 477);
+            button3.Name = "button3";
+            button3.Size = new Size(189, 39);
+            button3.TabIndex = 2;
+            button3.Text = "الرجوع";
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Panel_Contanier
+            // 
+            Panel_Contanier.Dock = DockStyle.Fill;
+            Panel_Contanier.Location = new Point(189, 0);
+            Panel_Contanier.Name = "Panel_Contanier";
+            Panel_Contanier.Size = new Size(531, 516);
+            Panel_Contanier.TabIndex = 1;
+            // 
+            // Frm_Add_Edit_Student
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(46, 46, 46);
+            ClientSize = new Size(720, 516);
+            Controls.Add(Panel_Contanier);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Frm_Add_Edit_Student";
+            RightToLeft = RightToLeft.Yes;
+            RightToLeftLayout = true;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Frm_Add_Edit_Grades";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Panel Panel_Contanier;
+    }
+}
---
+++ SchoolManagment/GUI/StudentsGUI/Frm_Add_Edit_Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManagment.GUI.StudentsGUI
+{
+    public partial class Frm_Add_Edit_Student : Form
+    {
+        public Frm_Add_Edit_Student()
+        {
+            InitializeComponent();
+        }
+    }
+}
